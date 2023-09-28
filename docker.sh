13ac82aa9aa742d70804500d52ef02c820c0fb7c
samiulislam16 <88020360+samiulislam16@users.noreply.github.com>
2023-09-28 11:26:29 +0600

    Create docker.sh
    install docker in debian

---
+++ docker.sh
@@ -0,0 +1,1 @@
+apt update && apt upgrade -y && apt install docker docker-compose -y && docker pull portainer/portainer-ce:latest ; docker run -d -p 8000:8000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest ; ip a
